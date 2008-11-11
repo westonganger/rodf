49d39ce83f802a954634e67666957d17a4f973fb
thiago.arrais <thiago.arrais@gmail.com>
2008-11-11 15:39:50 -0300

    Spec for spreadsheet structure (at least that's what I could get to with hpricto's XPath processor)
    darcs-hash:20081111183950-49d33-34037b8bf6b1f23cd4f0f8520556ea076a26af59.gz

--- test/spreadsheet_spec.rb
+++ test/spreadsheet_spec.rb
@@ -1,6 +1,20 @@
 $:.unshift(File.expand_path(File.dirname(__FILE__) + '/../lib'))
 
+require 'rspec_hpricot_matchers'
+include RspecHpricotMatchers
+
 require 'odf/spreadsheet'
 
 describe ODF::SpreadSheet do
+  it "should have the expected structure" do
+    output = ODF::SpreadSheet.create {|s| }
+    output.should have_tag('//office:document-content/*')
+    output.should have_tag('//office:body/*')
+    output.should have_tag('//office:spreadsheet')
+  end
+
+  it "should be empty if no tables were added" do
+    output = ODF::SpreadSheet.create {|s| }
+    output.should have_tag('//office:spreadsheet/*', :count => 0)
+  end
 end
