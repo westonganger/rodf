f822ae82616a282e676839da58a989a8f32727db
thiago.arrais <thiago.arrais@gmail.com>
2008-11-11 15:40:47 -0300

    Now able to output empty spreadsheets
    darcs-hash:20081111184047-49d33-04dad7677fa7b753005c8fafede5e37d1c77538b.gz

--- lib/odf/spreadsheet.rb
+++ lib/odf/spreadsheet.rb
@@ -1,4 +1,29 @@
+require 'builder'
+
 module ODF
   class SpreadSheet
+    def self.create
+      s = new
+      yield s if block_given?
+      s.content
+    end
+    
+    def content
+      b = Builder::XmlMarkup.new
+
+      b.instruct! :xml, :version => '1.0', :encoding => 'UTF-8'
+      b.tag! 'office:document-content', 'xmlns:office' => "urn:oasis:names:tc:opendocument:xmlns:office:1.0",
+                                        'xmlns:table' => "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
+                                        'xmlns:text' => "urn:oasis:names:tc:opendocument:xmlns:text:1.0",
+                                        'xmlns:oooc' => "http://openoffice.org/2004/calc",
+                                        'xmlns:style' => "urn:oasis:names:tc:opendocument:xmlns:style:1.0",
+                                        'xmlns:fo' => "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" do
+      |xml|
+        xml.office:body do
+          xml.office:spreadsheet do
+          end
+        end
+      end
+    end
   end
 end
