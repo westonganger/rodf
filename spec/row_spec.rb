1778efdec899e30b53e7502362bbe56ccd4795d5
thiago.arrais <thiago.arrais@gmail.com>
2008-11-12 15:36:31 -0300

    Table rows should have cells
    darcs-hash:20081112183631-49d33-6f59f4aecfcf3062fa8c50b05ada3557f5187644.gz

---
+++ spec/row_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+require 'odf/spreadsheet'
+
+describe ODF::Row do
+  it "should allow cells to be added" do
+    output = ODF::Row.create
+    output.should have_tag('//table:table-row')
+    output.should_not have_tag('//table:table-row/*')
+
+    output = ODF::Row.create {|r|
+      r.cell
+      r.cell
+    }
+    output.should have_tag('//table:table-row/*', :count => 2)
+    output.should have_tag('//table:table-cell')
+  end
+end
