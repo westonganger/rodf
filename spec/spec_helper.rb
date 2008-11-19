b8a59014320532692184ad415498380285150ff9
Thiago Arrais <thiago.arrais@gmail.com>
2008-11-19 06:59:58 -0300

    Spec code has license boilerplate text too >:-|

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,13 +7,13 @@
 # published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
 
-# Foobar is distributed in the hope that it will be useful,
+# rODF is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 
 # You should have received a copy of the GNU Lesser General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with rODF.  If not, see <http://www.gnu.org/licenses/>.
 
 $:.unshift(File.expand_path(File.dirname(__FILE__) + '/../lib'))
 
